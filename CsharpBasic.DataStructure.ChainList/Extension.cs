a01f7432f2e8057c86dadf5b1784b8d76765a312
wenql <412826828@qq.com>
2018-02-24 15:00:56 +0800

    链表

---
+++ CsharpBasic.DataStructure.ChainList/Extension.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpBasic.DataStructure.ChainList
+{
+    public static class Extension
+    {
+    }
+}
---
+++ CsharpBasic.DataStructure.ChainList/Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpBasic.DataStructure.ChainList
+{
+    public class Model<T>
+    {
+        public T Data { get; set; }
+        public Model<T> Next { get; set; }
+    }
+}
